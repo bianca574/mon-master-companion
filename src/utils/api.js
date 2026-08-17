e11776312cb921bb77e1cd859d716a5585180517
Bianca-Ioana Ilie-Serban <bianca-ioana.ilie-serban@etu.u-paris.fr>
2026-08-17 16:14:53 +0300

    Connect the deployed repos

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import { getToken, logout } from './auth';
 
-const API_BASE = 'http://localhost:3001';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3001';
 
 async function apiFetch(path, options = {}) {
     const token = getToken();
